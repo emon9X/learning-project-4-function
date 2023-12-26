41755f2c538991c8ff9649e075aec74039418de8
emon9X <shahariar.emn990@gmail.com>
2023-12-26 16:54:22 +0600

    implicit function 2

--- function.js
+++ function.js
@@ -13,5 +13,5 @@ function myfunction(parameter1, parameter2) {
     parameter1+parameter2;
 }
 let x = myfunction(10,5) /*here 10 and 5 are called argument passed to the function as parameter */
-console.log(x); 
+console.log(x); /* here x is undefined. no value is returned explicitly from the function*/
 }
