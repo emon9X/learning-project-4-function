5d71ec8639b8abc9f7634039c7580fb231587d1a
unknown <shahariar.emn990@gmail.com>
2023-12-26 12:45:02 +0600

    implicit return

---
+++ function.js
@@ -0,0 +1,17 @@
+{
+    function myfunction(parameter1, parameter2) {
+    return parameter1+parameter2;
+}
+let x = myfunction(10,5) /*here 10 and 5 are called argument passed to the function as parameter */
+console.log(x);
+}
+
+{
+//implicit return
+
+function myfunction(parameter1, parameter2) {
+    parameter1+parameter2;
+}
+let x = myfunction(10,5) /*here 10 and 5 are called argument passed to the function as parameter */
+console.log(x); 
+}
